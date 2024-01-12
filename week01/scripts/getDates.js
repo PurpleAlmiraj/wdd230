21aec1c63c24884bc4ec09c7cd281cb63f9fafd0
Purple Almiraj <Ryanbluej@gmail.com>
2024-01-12 12:23:01 -0800

    Delete week01 directory

--- week01/scripts/getDates.js
+++
@@ -1,9 +0,0 @@
-let currentYear = new Date().getFullYear();
-
-document.getElementById('currentYear').textContent = currentYear;
-
-let oLastModif = new Date(document.lastModified);
-
-let formattedDate = oLastModif.toLocaleString('en-US');
-
-document.getElementById('lastModified').textContent = formattedDate;
