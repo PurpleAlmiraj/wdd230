7530fe467898f49f5a546fb6305eb840a81cc48e
Ryan Jones <ryanbluej@gmail.com>
2024-02-16 10:45:21 -0800

    Windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,6 @@
 var city = 'Rexburg';
 var state = 'ID';
-var apiKey = '55cf2d37fd795db3e7ddc43034d164f1'; // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+var apiKey = '815166eec2fc99695f124fe9928dde2a'; // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}&units=metric`;
 
 fetch(apiUrl)
@@ -19,3 +19,19 @@ fetch(apiUrl)
         document.getElementById('windSpeed').textContent = windSpeed;
     })
     .catch(error => console.error('Error fetching weather:', error));
+
+
+
+var temperatureCelsius = 10;
+var windSpeedMetersPerSecond = 5;
+var temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+var windSpeedMilesPerHour = windSpeedMetersPerSecond * 2.237;
+document.getElementById('temperatureFahrenheit').textContent = temperatureFahrenheit;
+document.getElementById('windSpeedMilesPerHour').textContent = windSpeedMilesPerHour;
+if (windSpeedMilesPerHour > 3 && windSpeedMilesPerHour < 50) {
+    var windChill = 35.74 + (0.6215 * temperatureFahrenheit) - (35.75 * Math.pow(windSpeedMilesPerHour, 0.16)) + (0.4275 * temperatureFahrenheit * Math.pow(windSpeedMilesPerHour, 0.16));
+
+    windChill = Math.round(windChill * 100) / 100;
+
+    document.getElementById('windChill').textContent = windChill;
+}
