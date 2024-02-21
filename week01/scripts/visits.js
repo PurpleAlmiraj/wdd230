b242677bd5a62b5a66ba28610f783089bb021a96
Ryan Jones <ryanbluej@gmail.com>
2024-02-21 11:19:54 -0800

    visits again

--- week01/scripts/visits.js
+++ week01/scripts/visits.js
@@ -1,18 +1,18 @@
 const visits = document.querySelector(".numvisits");
 
-// Retrieve the number of visits from localStorage
+
 let numVisits = Number(localStorage.getItem("visits-ls"));
 
-// If numVisits is null or undefined, set it to 0
+
 if (isNaN(numVisits)) {
     numVisits = 0;
 }
 
-// Update the text content of the visits element
+
 visits.textContent = numVisits;
 
-// Increment numVisits
+
 numVisits++;
 
-// Store the updated numVisits back into localStorage
+
 localStorage.setItem("visits-ls", numVisits);
