3841a9a2187150e8aa281b09283816cf12d5810e
Ryan Jones <ryanbluej@gmail.com>
2024-03-23 10:19:43 -0700

    Darkmode?

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -5,22 +5,21 @@ const darkmodebutton = document.querySelector('#dark');
 let isDarkMode = localStorage.getItem('darkMode') === 'true';
 
 // Apply initial dark mode state on page load
-body.classList.toggle('darkness', isDarkMode);
-darkmodebutton.classList.toggle('darkness', isDarkMode);
-toggleForecastItems(isDarkMode);
+toggleDarkMode(isDarkMode);
 
 darkmodebutton.addEventListener('click', () => {
     isDarkMode = !isDarkMode;
-
-    body.classList.toggle('darkness', isDarkMode);
-    darkmodebutton.classList.toggle('darkness', isDarkMode);
-    
-    // Toggle the class for the forecast items
-    toggleForecastItems(isDarkMode);
+    toggleDarkMode(isDarkMode);
 
     // Save dark mode state to local storage
     localStorage.setItem('darkMode', isDarkMode);
 });
+
+function toggleDarkMode(isDarkMode) {
+    body.classList.toggle('darkness', isDarkMode);
+    darkmodebutton.classList.toggle('darkness', isDarkMode);
+    toggleForecastItems(isDarkMode);
+}
 
 function toggleForecastItems(isDarkMode) {
     const forecastItems = document.querySelectorAll('.forecast-item');
